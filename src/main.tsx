d8015153023c937b241fe96c2d3ef360756049d8
Webcanbe <minsihu2@gmail.com>
2026-09-17 19:56:08 +0900

    Apply Phase 4 UX overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./globals.css"
 import App from "./App"
+import "./phase4.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
