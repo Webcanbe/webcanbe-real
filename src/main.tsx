96f102cda71df007de1d0eb828bef057cd2a8a8e
Webcanbe <minsihu2@gmail.com>
2026-09-23 15:44:42 +0900

    Install global privacy-safe link tracking

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import App from "./App"
 import "./phase4-final-ui.css"
 import "./app-shell.css"
 import "./editor-shell.css"
-import { initializeAnalytics } from "./analytics"
+import { initializeAnalytics, installLinkTracking } from "./analytics"
 
 if (window.location.pathname === "/requests") void import("./my-requests.css")
 
+installLinkTracking()
 void initializeAnalytics()
 
 createRoot(document.getElementById("root")!).render(
